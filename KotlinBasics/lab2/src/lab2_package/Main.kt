e8a00b2d18f5ce6817706f6e5486b344a1aedd95
Balogh-Laszlo <79311989+Balogh-Laszlo@users.noreply.github.com>
2021-09-27 15:40:51 +0300

    Create Main.kt
    2021.09.27 commit

---
+++ KotlinBasics/lab2/src/lab2_package/Main.kt
@@ -0,0 +1,16 @@
+package lab2_package
+
+fun main(){
+    val dict: IDictionary = ListDictionary
+    println("Number of words: ${dict.size()}")
+    var word: String?
+    while(true){
+        print("What to find? ")
+        word = readLine()
+        if( word.equals("quit")){
+            break
+        }
+        println("Result: ${word?.let { dict.find(it) }}")
+    }
+
+}
