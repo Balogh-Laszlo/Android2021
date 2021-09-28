9f32ea51e39aec2500c27fe1e5d7aa69b9efb693
Balogh-Laszlo <79311989+Balogh-Laszlo@users.noreply.github.com>
2021-09-28 16:39:10 +0300

    Create DictionaryProvider.kt
    2021.09.28

---
+++ KotlinBasics/lab2/src/lab2_package/DictionaryProvider.kt
@@ -0,0 +1,13 @@
+package lab2_package
+
+class DictionaryProvider {
+    companion object {
+        fun createDictionary(type: DictionaryType): IDictionary {
+            return when (type) {
+                DictionaryType.ARRAY_LIST -> ListDictionary
+                DictionaryType.TREE_SET -> TreeSetDictionary
+                DictionaryType.HASH_SET -> HashSetDictionary
+            }
+        }
+    }
+}
