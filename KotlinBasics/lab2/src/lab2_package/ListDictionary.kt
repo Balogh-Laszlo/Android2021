5493b5b3ed9b795be0132f8fe8fb60cdfad87554
Balogh-Laszlo <79311989+Balogh-Laszlo@users.noreply.github.com>
2021-09-28 16:39:21 +0300

    Update ListDictionary.kt
    2021.09.28

--- KotlinBasics/lab2/src/lab2_package/ListDictionary.kt
+++ KotlinBasics/lab2/src/lab2_package/ListDictionary.kt
@@ -6,15 +6,9 @@ object ListDictionary : IDictionary {
     private val words : MutableList<String> = mutableListOf()
 
     init {
-        File("C:\\Users\\lacob\\AndroidStudioProjects\\Android2021\\KotlinBasics\\lab2\\src\\lab2_package\\in.txt").forEachLine { words.add(it) }
+        File("C:\\Users\\lacob\\AndroidStudioProjects\\Android2021\\KotlinBasics\\lab2\\src\\lab2_package\\in.txt").forEachLine { this.add(it) }
     }
-    override fun add(word: String): Boolean {
-        if(words.add(word)){
-            return true
-        }
-        return false
-
-    }
+    override fun add(word: String): Boolean = words.add(word)
 
     override fun find(word: String): Boolean = words.contains(word)
 
